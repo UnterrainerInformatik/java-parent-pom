e231865ea5d9a77f7d40ef24ffd5aef02a76b7d0
Psilo <psilo@unterrainer.info>
2020-07-06 15:20:55 +0200

    add version fetching
    advance version

---
+++ src/main/java/info/unterrainer/commons/parentpom/Information.java
@@ -0,0 +1,6 @@
+package info.unterrainer.commons.parentpom;
+
+public class Information {
+	public static final String BuildTime = "2020-07-06T13:11:38Z";
+	public static final String PomVersion = "0.0.21";
+}
