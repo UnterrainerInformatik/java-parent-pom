396d37014741710e3b722af33efe75ea758ce72c
Psilo <psilo@unterrainer.info>
2020-07-07 08:41:42 +0200

    version bump

--- src/main/java/info/unterrainer/commons/parentpom/Information.java
+++ src/main/java/info/unterrainer/commons/parentpom/Information.java
@@ -1,6 +1,7 @@
 package info.unterrainer.commons.parentpom;
 
 public class Information {
-	public static final String BuildTime = "2020-07-06T13:11:38Z";
-	public static final String PomVersion = "0.0.21";
+	public static final String name = "parentpom";
+	public static final String buildTime = "2020-07-06T14:11:50Z";
+	public static final String pomVersion = "0.0.21";
 }
