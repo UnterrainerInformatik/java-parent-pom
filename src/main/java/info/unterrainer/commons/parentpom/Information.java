0a6fea0deaa2591c45de9e316a35d6a21b6dec38
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-09-06 23:52:47 +0200

    Travis try 1

--- src/main/java/info/unterrainer/commons/parentpom/Information.java
+++ src/main/java/info/unterrainer/commons/parentpom/Information.java
@@ -1,7 +1,7 @@
 package info.unterrainer.commons.parentpom;
 
 public class Information {
-	public static final String name = "parentpom";
-	public static final String buildTime = "2020-07-06T14:11:50Z";
-	public static final String pomVersion = "0.0.21";
+	public static final String name = "Parent-POM";
+	public static final String buildTime = "2020-08-20T06:44:27Z";
+	public static final String pomVersion = "0.0.22";
 }
