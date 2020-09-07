54839b45171d23ab21b5b100f522b8414db51781
Psilo <psilo@unterrainer.info>
2020-09-07 15:42:44 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/info/unterrainer/commons/parentpom/Information.java
+++ src/main/java/info/unterrainer/commons/parentpom/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.parentpom;
 
 public class Information {
 	public static final String name = "Parent-POM";
-	public static final String buildTime = "2020-08-20T06:44:27Z";
+	public static final String buildTime = "2020-07-13T15:40:38Z";
 	public static final String pomVersion = "0.0.22";
 }
