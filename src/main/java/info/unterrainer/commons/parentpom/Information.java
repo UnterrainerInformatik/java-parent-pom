8785387162ec74cb4c16812df3cf52de3f6ff4c3
Psilo <psilo@unterrainer.info>
2020-10-21 09:54:45 +0200

    switch junit to junit5
    bump version

--- src/main/java/info/unterrainer/commons/parentpom/Information.java
+++ src/main/java/info/unterrainer/commons/parentpom/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.parentpom;
 
 public class Information {
 	public static final String name = "Parent-POM";
-	public static final String buildTime = "2020-07-13T15:40:38Z";
-	public static final String pomVersion = "0.0.22";
+	public static final String buildTime = "2020-10-21T07:34:48Z";
+	public static final String pomVersion = "0.0.27";
 }
