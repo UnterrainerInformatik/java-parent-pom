8b33cd2a2a33107022a99f0923bd24a53b983d07
Psilo <psilo@unterrainer.info>
2020-11-03 17:14:49 +0100

    add .gitattributes file

--- src/main/java/info/unterrainer/commons/parentpom/Information.java
+++ src/main/java/info/unterrainer/commons/parentpom/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.parentpom;
 
 public class Information {
 	public static final String name = "Parent-POM";
-	public static final String buildTime = "2020-10-22T08:19:50Z";
-	public static final String pomVersion = "0.1.2";
+	public static final String buildTime = "2020-10-22T12:50:40Z";
+	public static final String pomVersion = "0.1.3";
 }
