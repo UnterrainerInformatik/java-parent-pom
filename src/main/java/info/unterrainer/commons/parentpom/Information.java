f9220c1fc781a1350e1a1b14f55a8134722ca3f0
Psilo <psilo@unterrainer.info>
2021-03-10 18:14:52 +0100

    update, fix and refactor lombok entry

--- src/main/java/info/unterrainer/commons/parentpom/Information.java
+++ src/main/java/info/unterrainer/commons/parentpom/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.parentpom;
 
 public class Information {
 	public static final String name = "Parent-POM";
-	public static final String buildTime = "2021-03-10T17:05:43Z";
-	public static final String pomVersion = "0.1.4";
+	public static final String buildTime = "2021-03-10T17:14:07Z";
+	public static final String pomVersion = "0.1.5";
 }
