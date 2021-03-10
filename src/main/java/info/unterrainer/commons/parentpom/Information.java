effbc900a31863377363bc09a3c2cee8705ec309
Psilo <psilo@unterrainer.info>
2021-03-10 18:06:04 +0100

    update lombok

--- src/main/java/info/unterrainer/commons/parentpom/Information.java
+++ src/main/java/info/unterrainer/commons/parentpom/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.parentpom;
 
 public class Information {
 	public static final String name = "Parent-POM";
-	public static final String buildTime = "2020-10-22T12:50:40Z";
-	public static final String pomVersion = "0.1.3";
+	public static final String buildTime = "2021-03-10T17:05:43Z";
+	public static final String pomVersion = "0.1.4";
 }
